4d40541f45b369d03cfbe1ead024b29fd45cc568
Harsh Kumar <harsh.kumar@powerschool.com>
2024-12-25 23:41:21 +0530

    Add the CSS configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        xs: "0.625rem",
+        sm: "0.75rem",
+        base: "0.875rem",
+        large: "1.125rem",
+      },
+      spacing: {
+        1: "5px",
+        2: "10px",
+        3: "15px",
+        4: "20px",
+      },
+    },
   },
   plugins: [],
 };
